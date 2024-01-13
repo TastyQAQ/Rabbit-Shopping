075af0d4852d86c8c9dba33a3f6c83b94b069ffc
TastyQAQ <aq22171280@gmail.com>
2024-01-13 21:07:32 +0800

    新鮮好物結構樣式及渲染

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,5 +1,9 @@
 import request from '@/utils/http'
-
+// 獲取輪播圖
 export function getBannerImg() {
     return request.get('/home/banner')
+}
+// 獲取新鮮好物數據
+export function getNewProduct() {
+    return request.get('/home/new')
 }
