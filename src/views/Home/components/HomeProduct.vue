7ee3fa82628129d8c589c671a3458339d3091a1f
TastyQAQ <aq22171280@gmail.com>
2024-01-14 20:08:37 +0800

    產品列表封裝組件

--- src/views/Home/components/HomeProduct.vue
+++ src/views/Home/components/HomeProduct.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HomePanel from './HomePanel.vue'
+import GoodsItem from '@/components/GoodsItem.vue'
 import { getGoodsList } from '@/apis/home'
 import { ref } from 'vue'
 
@@ -23,13 +24,8 @@ getGoods()
                     </strong>
                 </router-link>
                 <ul class="goods-list">
-                    <li v-for="i in item.goods" :key="i.id">
-                        <router-link to="/" class="goods-item">
-                            <img v-img-lazy="i.picture" alt="">
-                            <p class="name ellipsis">{{i.name}}</p>
-                            <p class="desc ellipsis">{{i.desc}}</p>
-                            <p class="price"><i>$</i>{{ i.price }}</p>
-                        </router-link>
+                    <li v-for="goods in item.goods" :key="goods.id">
+                        <GoodsItem :goods="goods"></GoodsItem>
                     </li>
                 </ul>
             </div>
@@ -124,41 +120,6 @@ getGoods()
       }
     }
 
-    .goods-item {
-      display: block;
-      width: 220px;
-      padding: 20px 30px;
-      text-align: center;
-      transition: all .5s;
-
-      &:hover {
-        transform: translate3d(0, -3px, 0);
-        box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
-      }
-
-      img {
-        width: 160px;
-        height: 160px;
-      }
-
-      p {
-        padding-top: 10px;
-      }
-
-      .name {
-        font-size: 16px;
-      }
-
-      .desc {
-        color: #999;
-        height: 29px;
-      }
-
-      .price {
-        color: $priceColor;
-        font-size: 20px;
-      }
-    }
   }
 }
 </style>
