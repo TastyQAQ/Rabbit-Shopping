e0e70a9e47f76afaded4013d9730959c5596760b
TastyQAQ <aq22171280@gmail.com>
2024-01-20 20:57:26 +0800

    創建購物車倉庫

---
+++ src/stores/cart.js
@@ -0,0 +1,25 @@
+import { defineStore } from "pinia"
+import { ref } from 'vue'
+
+export const useCartStore = defineStore('cart', () => {
+    const cartList = ref([])
+    const addCart = (goods) => {
+        // 根據商品的skuId查看傳入的商品是否已存在購物車內
+        const item = cartList.value.find(item => {
+            goods.skuId === item.skuId
+        })
+        // 若商品已在購物車內, 則商品數量加1
+        if(item) {
+            item.count++
+            // 若商品不在購物車內, 則將商品添加至購物車
+        } else {
+            cartList.value.push(goods)
+        }
+    }
+    return {
+        cartList,
+        addCart
+    }
+}, {
+  persist: true
+})
